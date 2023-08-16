39cfda5a62dd55448a0edad49aa03651bd4f7a34
Maximiliam Berggren <berggrenmille@hotmail.se>
2023-08-16 19:48:39 +0000

    Updated NGIN CMakeDependencies
    All Dependencies will now first look for specified dependencies in NGIN/thirdparty folder (Only on WINDOWS)

--- NGIN/CMakeDependencies.cmake
+++ NGIN/CMakeDependencies.cmake
@@ -2,6 +2,18 @@
 include(FetchContent)
 
 macro(fetch_dependency _name _repo _tag)
+    # Define the local path for the dependency
+    set(local_path "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/${_name}")
+
+    # Append the local path to the CMAKE_PREFIX_PATH if it exists and the package hasn't been found yet
+    if(EXISTS ${local_path} AND NOT ${_name}_FOUND)
+        list(APPEND CMAKE_PREFIX_PATH ${local_path})
+    endif()
+
+    # Try to find the package
+    find_package(${_name} QUIET)
+
+    # If the package isn't found, use FetchContent to obtain it
     if(NOT ${_name}_FOUND)
         FetchContent_Declare(${_name}
             GIT_REPOSITORY ${_repo}
@@ -10,6 +22,7 @@ macro(fetch_dependency _name _repo _tag)
         FetchContent_MakeAvailable(${_name})
     endif()
 endmacro()
+
 
 # fmtlib
 find_package(fmt 10 QUIET)
