eda95ad0c058f68468034bfdfaff87f686a296b3
Maximiliam Berggren <berggrenmille@hotmail.se>
2023-09-20 01:57:08 +0200

    Added Delegate
    Comparable to std::function

--- NGIN/include/NGIN/EntryPoint.hpp
+++ NGIN/include/NGIN/EntryPoint.hpp
@@ -5,20 +5,26 @@
 #include <NGIN/Logging/FileSink.hpp>
 #include <NGIN/Logging.hpp>
 
-#include <NGIN/Meta/StoragePolicy.hpp>
-#include <algorithm>
-#include <random>
+#include <NGIN/Util/Delegate.hpp>
+#include <chrono>
 
 #include <SDL2/SDL.h>
 
-struct HeavyStruct
+void freeFunction()
 {
-	int a, b, c, d, e;
-	//
+	volatile int a = 1;
+}
 
-	int heavyCalculation() const
+int simpleFunction(int a, int b)
+{
+	return a + b;
+}
+
+struct Test
+{
+	int simpleFunction(int a, int b)
 	{
-		return a * a + b * b + c * c + d * d + e * e;
+		return a + b;
 	}
 };
 
@@ -45,6 +51,82 @@ namespace NGIN
 		Logging::Init();
 		// Init Config
 		Config::Init();
+
+		const int NUM_ITERATIONS = 100000000; // or whatever large number you want
+
+		// Set up the std::function and Delegate objects
+		std::function<int(int, int)> stdFunc = simpleFunction;
+		NGIN::Util::Delegate del(simpleFunction);
+		Test test;
+		std::function<int(int, int)> stdFunc2 = std::bind(&Test::simpleFunction, &test, std::placeholders::_1, std::placeholders::_2);
+		NGIN::Util::Delegate del2(&Test::simpleFunction, &test);
+
+		std::function<void()> stdFunc3 = freeFunction;
+		NGIN::Util::Delegate del3(freeFunction);
+
+		// Benchmark std::function
+		auto start1 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			stdFunc(1, 2);
+		}
+		auto end1 = std::chrono::high_resolution_clock::now();
+		auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count();
+
+		// Benchmark Delegate
+		auto start2 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			del(1, 2);
+		}
+		auto end2 = std::chrono::high_resolution_clock::now();
+		auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count();
+
+		// Benchmark Delegate to member function
+
+		auto start3 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			del2(1, 2);
+		}
+		auto end3 = std::chrono::high_resolution_clock::now();
+		auto duration3 = std::chrono::duration_cast<std::chrono::milliseconds>(end3 - start3).count();
+
+		// Benchmark std::function to member function
+		auto start4 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			stdFunc2(1, 2);
+		}
+		auto end4 = std::chrono::high_resolution_clock::now();
+		auto duration4 = std::chrono::duration_cast<std::chrono::milliseconds>(end4 - start4).count();
+
+		// Benchmark std::function to free function
+		auto start5 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			stdFunc3();
+		}
+		auto end5 = std::chrono::high_resolution_clock::now();
+		auto duration5 = std::chrono::duration_cast<std::chrono::milliseconds>(end5 - start5).count();
+
+		// Benchmark Delegate to free function
+
+		auto start6 = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < NUM_ITERATIONS; ++i)
+		{
+			del3();
+		}
+		auto end6 = std::chrono::high_resolution_clock::now();
+		auto duration6 = std::chrono::duration_cast<std::chrono::milliseconds>(end6 - start6).count();
+
+		// Print results
+		std::cout << "std::function took: " << duration1 << "ms" << std::endl;
+		std::cout << "std::function to member function took: " << duration4 << "ms" << std::endl;
+		std::cout << "std::function to free function took: " << duration5 << "ms" << std::endl;
+		std::cout << "Delegate took: " << duration2 << "ms" << std::endl;
+		std::cout << "Delegate to member function took: " << duration3 << "ms" << std::endl;
+		std::cout << "Delegate to free function took: " << duration6 << "ms" << std::endl;
 
 		NGIN_WARNING("Initializing MTETSTTST {}", Config::GetRawValue("TEST"));
 
---
+++ NGIN/include/NGIN/Util/Delegate.hpp
@@ -0,0 +1,108 @@
+#pragma once
+#include <NGIN/Defines.hpp>
+#include <type_traits>
+#include <NGIN/Meta/StoragePolicy.hpp>
+#include <functional>
+namespace NGIN::Util
+{
+    class Delegate
+    {
+
+    public:
+        Delegate() : storage(), invoker(nullptr), destroyer(nullptr){};
+
+        // Handling Free Functions and Non-capturing Lambdas
+        template <typename F>
+        Delegate(F &&f) noexcept
+        {
+            using CallableType = std::decay_t<F>;
+
+            storage = StorageType(CallableType(std::forward<F>(f)));
+            invoker = reinterpret_cast<InvokerFunc>(&InvokeCallable<CallableType>);
+            destroyer = &DestroyCallable<CallableType>;
+        }
+
+        template <typename R, typename... Args>
+        Delegate(R (*func)(Args...)) noexcept
+        {
+
+            using FuncType = R (*)(Args...);
+            storage = StorageType(FuncType(func));
+
+            invoker = reinterpret_cast<InvokerFunc>(&InvokeCallable<FuncType, Args...>);
+            destroyer = &DestroyCallable<FuncType>;
+        }
+
+        // Handling Member Functions
+        template <class T, typename R, typename... Args>
+        Delegate(R (T::*func)(Args...), T *obj) noexcept
+        {
+            auto wrapper = [func, obj](Args... args)
+            {
+                return (obj->*func)(std::forward<Args>(args)...);
+            };
+
+            using WrapperType = decltype(wrapper);
+
+            storage = StorageType(std::move(wrapper));
+            invoker = reinterpret_cast<InvokerFunc>(&InvokeCallable<WrapperType, Args...>);
+            destroyer = &DestroyCallable<WrapperType>;
+        }
+
+        ~Delegate()
+        {
+            if (destroyer)
+            {
+                destroyer(storage.get());
+            }
+        }
+
+        template <typename... Args>
+        auto operator()(Args &&...args) -> decltype(auto)
+        {
+            if (!invoker)
+            {
+                throw std::runtime_error("Delegate not initialized");
+            }
+            void *args_arr[] = {&args...};
+            return invoker(storage.get(), args_arr);
+        }
+
+    private:
+        static constexpr size_t BUFFER_SIZE = sizeof(void *) * 4;
+
+        using StorageType = Meta::StoragePolicy::HybridStorage<BUFFER_SIZE>;
+        using InvokerFunc = void (*)(void *, void *[]);
+        using DestroyerFunc = void (*)(void *);
+
+        StorageType storage;
+        InvokerFunc invoker = nullptr;
+        DestroyerFunc destroyer = nullptr;
+
+        template <typename Callable, typename... Args>
+        static void InvokeCallable(void *storage, void *args_arr[])
+        {
+            auto &callable = *static_cast<Callable *>(storage);
+            InvokeImpl<Callable, Args...>(callable, args_arr);
+        }
+
+        template <typename Callable, typename First, typename... Rest>
+        static void InvokeImpl(Callable &callable, void *args_arr[])
+        {
+            callable(*static_cast<First *>(args_arr[0]), *static_cast<Rest *>(args_arr[1])...);
+        }
+
+        template <typename Callable>
+        static void InvokeImpl(Callable &callable, void *args_arr[])
+        {
+            callable();
+        }
+
+        template <typename Callable>
+        static void DestroyCallable(void *storage)
+        {
+            auto &callable = *static_cast<Callable *>(storage);
+            callable.~Callable();
+        }
+    };
+}
