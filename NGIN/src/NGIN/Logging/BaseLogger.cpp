bdcd2f4af2c24775a7bbb307eaa1e97e5e757487
berggrenmille <berggrenmille@hotmail.se>
2023-10-04 01:13:27 +0200

    Refactored FileSink and BaseLogger

--- NGIN/src/NGIN/Logging/BaseLogger.cpp
+++ NGIN/src/NGIN/Logging/BaseLogger.cpp
@@ -2,18 +2,27 @@
 #include <NGIN/Time.hpp>
 
 #include <HideWarnings/fmt.hpp>
+
 namespace NGIN::Logging
 {
-	std::string BaseLogger::FormatLogMessage(Level level, const std::string &message, const std::source_location &location)
-	{
-		auto file = Util::ExtractFileName(location.file_name());
-		auto timeNow = Time::GetCurrentTime();
+    String BaseLogger::FormatLogMessage(Level level,
+                                        const String& message,
+                                        const std::source_location& location)
+    {
+        auto file = Util::ExtractFileName(location.file_name());
+        auto timeNow = Time::GetCurrentTime();
 
-		return Util::Format("{:%Y-%m-%d %H:%M:%S} | {:>20}:{:<5} | {:<8} | {}",
-							fmt::localtime(timeNow),
-							file,
-							location.line(),
-							LogLevelToString(level),
-							message);
-	}
+        return Util::Format("{:%Y-%m-%d %H:%M:%S} | {:>20}:{:<5} | {:<8} | {}",
+                            fmt::localtime(timeNow),
+                            file,
+                            location.line(),
+                            LogLevelToString(level),
+                            message);
+    }
+
+    void BaseLogger::Log(Level level, const String& message, const std::source_location& location)
+    {
+
+        LogInternal(level, FormatLogMessage(level, message, location));
+    }
 }
