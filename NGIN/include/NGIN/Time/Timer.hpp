c8f9110100c7861761fa57bb602a746560f2d766
berggrenmille <berggrenmille@hotmail.se>
2023-10-22 18:03:03 +0200

    Timer now now properly overhauled

--- NGIN/include/NGIN/Time/Timer.hpp
+++ NGIN/include/NGIN/Time/Timer.hpp
@@ -8,9 +8,9 @@
 namespace NGIN::Time
 {
     /// @class Timer
-    /// @brief A high-resolution timer used to measure durations.
-    ///
+    /// @brief A templated Timer class.
     /// @tparam ClockType The clock type to use for the timer. Ex: NGIN::Time::SteadyClock
+    /// @note The timer is started when it is constructed.
     template<typename ClockType>
     class Timer
     {
@@ -23,6 +23,8 @@ namespace NGIN::Time
         }
 
         /// @brief Gets the elapsed time since the timer was started or reset.
+        /// @tparam ReturnT The type to return the elapsed time as.
+        /// @tparam DurationT The duration type to return the elapsed time as.
         template<typename ReturnT = Int, typename DurationT = Seconds>
         [[nodiscard]] inline ReturnT Elapsed() const
         {
@@ -31,13 +33,18 @@ namespace NGIN::Time
             return static_cast<ReturnT>(elapsedF64);
         }
 
+        /// @brief Gets the elapsed time since the timer was started or reset.
+        /// @tparam DurationT The duration type to return the elapsed time as.
+        /// @return The elapsed time as a floating point number.
         template<typename DurationT = Seconds>
         [[nodiscard]] inline F64 ElapsedF64() const
         {
             return Elapsed<F64, DurationT>();
         }
 
-        //ElapsedInt
+        /// @brief Gets the elapsed time since the timer was started or reset.
+        /// @tparam DurationT The duration type to return the elapsed time as.
+        /// @return The elapsed time as an integer.
         template<typename DurationT = Seconds>
         [[nodiscard]] inline Int ElapsedInt() const
         {
