0fc791ce6816fc127aeb243c247a4e06a0d9c3a3
berggrenmille <berggrenmille@hotmail.se>
2023-10-22 18:13:50 +0200

    Documented Module.hpp

--- NGIN/include/NGIN/Core/Module.hpp
+++ NGIN/include/NGIN/Core/Module.hpp
@@ -7,32 +7,45 @@
 
 namespace NGIN::Core
 {
+    // Forward declarations
     class Engine;
 
-    template<typename... Ts>
-    struct DependencyWrapper {};
-
-
+    /// @class Module
+    /// @brief Base class for all modules
+    /// @details Modules are the building blocks of the engine.
+    ///          They are used to extend the engine's functionality.
+    ///          Modules can declare dependencies by overriding the NGIN::Core::Module::Dependencies alias
+    ///          The engine will then make sure that all dependencies are initialized before the module itself.
+    ///          Circular dependencies are not supported and will result in undefined behavior.
+    ///          Modules are initialized in the order of their dependencies.
+    ///          Modules are shutdown in the reverse order of their dependencies.
+    ///          It is recommended to loosely couple modules by using Events instead of using the modules directly.
     class Module
     {
     public:
         friend class Engine;
 
+        /// @brief Alias for declaring dependencies
+        /// @details Example:
+        /// using Dependencies = Meta::TypeWrapper<Module1, Module2, ...>;
         using Dependencies = Meta::TypeWrapper<void>;
 
-        NGIN_API Module() = default;
 
+        /// @brief Default constructor
+        NGIN_API Module() = default;
+        /// @brief Default destructor
         NGIN_API virtual ~Module() = default;
 
     protected:
+        /// @brief Called when the module is initialized
         NGIN_API virtual void OnInit(Engine* engine) {}
-
+        /// @brief Called when the module is shutdown
         NGIN_API virtual void OnShutdown() {}
-
+        /// @brief Called before the module is ticked
         NGIN_API virtual void OnPreTick(const F64 deltaTime) {}
-
+        /// @brief Called when the module is ticked
         NGIN_API virtual void OnTick(const F64 deltaTime) {}
-
+        /// @brief Called after the module is ticked
         NGIN_API virtual void OnPostTick(const F64 deltaTime) {}
 
 
